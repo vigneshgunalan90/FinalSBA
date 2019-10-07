2a726dc656ea00648b6f45f82a021a2a0d501d66
Vignesh <Vignesh.Gunalan@Chubb.com>
2019-10-07 19:37:27 +0800

    Second Version

--- ProjectManagerWebAPI/ProjectManager.DataLayer/ProjectManagerEF.Designer.cs
+++ ProjectManagerWebAPI/ProjectManager.DataLayer/ProjectManagerEF.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\334791\CTS\FSE\FSEFinals-master\ProjectManagerWebAPI\ProjectManager.DataLayer\ProjectManagerEF.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\334791\CTS\FSE\FinalSBA\FinalSBA\ProjectManagerWebAPI\ProjectManager.DataLayer\ProjectManagerEF.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
